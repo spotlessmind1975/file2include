60fe9a56da4cf886ace6ebaec53d4e9acb6d7135
Marco Spedaletti <asimov@mclink.it>
2021-01-10 01:43:13 +0100

    Changed license to Apache 2.0.

--- file2include.c
+++ file2include.c
@@ -1,11 +1,51 @@
+/*****************************************************************************
+ * IMG2MIDRES - Utility to convert images into midres pictures               *
+ *****************************************************************************
+ * Copyright 2020 Marco Spedaletti (asimov@mclink.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *----------------------------------------------------------------------------
+ * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+ * (la "Licenza"); è proibito usare questo file se non in conformità alla
+ * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+ * il software distribuito nei termini della Licenza è distribuito
+ * "COSÌ COM'È", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+ * implicite. Consultare la Licenza per il testo specifico che regola le
+ * autorizzazioni e le limitazioni previste dalla medesima.
+ ****************************************************************************/
+
 // This directive is used to deactivate the safety warning for the use 
 // of fopen under Microsoft Visual Studio.
 #pragma warning(disable : 4996)
+
+/****************************************************************************
+ ** INCLUDE SECTION
+ ****************************************************************************/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+/****************************************************************************
+ ** RESIDENT VARIABLES SECTION
+ ****************************************************************************/
+
+// Error levels.
 
 #define ERL_WRONG_OPTIONS               1
 #define ERL_MISSING_INPUT_FILENAME      2
@@ -38,9 +78,9 @@ int verbose = 0;
 // This function prints a short guide to the available options.
 void usage_and_exit(int _level, int _argc, char* _argv[]) {
 
-    printf("file2include - Convert one or more binary file in an include/source file.\n");
-    printf("Copyright(c) 2020 by Marco Spedaletti.\n");
-    printf("Licensed under CC-BY-NC-SA\n\n");
+    printf("file2include - Convert one or more binary file in an include/source file\n");
+    printf("Copyright(c) 2020-2021 by Marco Spedaletti.\n");
+    printf("Licensed under Apache 2.0 License\n\n");
     printf("Usage: %s [options]", _argv[0]);
     printf("\n");
     printf("options:\n");
